5c5246e35b22e39b0eb3b049248a66066111c3c5
Lucas Alves Dos Santos <lucas201273@gmail.com>
2021-02-08 17:42:33 -0300

    feat: add validation of SignIn Page

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
+import * as Yup from 'yup';
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import logoImg from '../../assets/logo.svg';
 import { Container, Content, Background } from './styles';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
+import getValidationErrors from '../../utils/getValidationErrors';
+
 const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+  const handleSubmit = useCallback(async (data: Object) => {
+    try {
+      formRef.current?.setErrors({});
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string()
+          .required('Email obrigatório')
+          .email('Informe um email válido'),
+        password: Yup.string().required('Senha obrigatória'),
+      });
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (error) {
+      const errors = getValidationErrors(error);
+      formRef.current?.setErrors(errors);
+    }
+  }, []);
+
   return (
     <>
       <Container>
         <Content>
           <img src={logoImg} alt="logo do GoBarber" />
-          <form>
+          <Form onSubmit={handleSubmit} ref={formRef}>
             <h1> Faça seu logon </h1>
             <Input
               name="email"
@@ -25,7 +50,7 @@ const SignIn: React.FC = () => {
             />
             <Button type="submit"> Entrar</Button>
             <a href="forgot"> Esqueci minha senha</a>
-          </form>
+          </Form>
           <a href="createAccount">
             <FiLogIn size={20} />
             Criar Conta
