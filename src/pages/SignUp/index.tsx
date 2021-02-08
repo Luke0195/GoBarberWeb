1da37ba5469cd92beca97df86910d8d7f5066a22
Lucas Alves Dos Santos <lucas201273@gmail.com>
2021-02-08 16:21:56 -0300

    fix: add correction on yup libary

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Form } from '@unform/web';
 import { FiUser, FiMail, FiLock, FiArrowLeft } from 'react-icons/fi';
-
+import * as Yup from 'yup';
 import imgLogo from '../../assets/logo.svg';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -9,9 +9,23 @@ import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
 const SignUp: React.FC = () => {
-  function handleSubmit(data: Object): void {
-    console.log(data);
-  }
+  const handleSubmit = useCallback(async (data: Object) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string()
+          .required('Email obrigatório')
+          .email('Informe um email válido'),
+        password: Yup.string().min(6, 'Minimo 6 caracteres'),
+      });
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
   return (
     <>
       <Container>
