fff2177eca345ef728e1629e0db593d69073a429
Lucas Alves Dos Santos <lucas201273@gmail.com>
2021-02-09 20:07:06 -0300

    feat: add create authContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import AuthContext from './context/AuthContext';
 import GlobalStyles from './styles/global';
 import SignIn from './pages/SignIn';
 
 const App: React.FC = () => {
   return (
     <>
-      <SignIn />
+      <AuthContext.Provider value={{ name: 'Lucas' }}>
+        <SignIn />
+      </AuthContext.Provider>
       <GlobalStyles />
     </>
   );
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useRef } from 'react';
+import React, { useCallback, useRef, useContext } from 'react';
 import * as Yup from 'yup';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
@@ -7,11 +7,14 @@ import logoImg from '../../assets/logo.svg';
 import { Container, Content, Background } from './styles';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
-
+import AuthContext from '../../context/AuthContext';
 import getValidationErrors from '../../utils/getValidationErrors';
 
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
+
+  const { name } = useContext(AuthContext);
+  console.log(name);
   const handleSubmit = useCallback(async (data: Object) => {
     try {
       formRef.current?.setErrors({});
