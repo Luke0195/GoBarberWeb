3b8ced5b05a0032fe4d00d20ee78f8e1788132e4
Lucas Alves Dos Santos <lucas201273@gmail.com>
2021-02-11 16:03:04 -0300

    feat: add toast-context

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
+
 import GlobalStyles from './styles/global';
 import SignIn from './pages/SignIn';
-import { AuthProvider } from './hooks/AuthContext';
 import ToastContainer from './components/ToastContainer';
+
+import { AuthProvider } from './hooks/auth';
+import { ToastProvider } from './hooks/toast';
 
 const App: React.FC = () => {
   return (
     <>
       <AuthProvider>
-        <SignIn />
+        <ToastProvider>
+          <SignIn />
+        </ToastProvider>
       </AuthProvider>
 
       <ToastContainer />
